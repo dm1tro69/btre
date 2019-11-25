8c4ec6fb2997efcaaef1d8b71e263c5fd9ecb893
dm1tro69 <dimolg22@gmail.com>
2019-11-25 20:02:28 +0200

    models

--- btre/settings.py
+++ btre/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
+        'NAME': 'btredb2',
         'USER': 'postgres',
         'PASSWORD': 'toor',
         'HOST': 'localhost',
