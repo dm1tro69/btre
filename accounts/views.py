4f7e2c6266a36dd9ec3a3b008d2983ccfb038edd
dm1tro69 <dimolg22@gmail.com>
2019-11-30 21:21:23 +0200

    register

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages, auth
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -14,8 +16,41 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        #Register_user
-        return
+        #Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #Check if paasword match
+
+        if password == password2:
+            #Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    #Looks good
+                    user = User.objects.create_user(username=username, password=password,
+                                                    email=email, first_name=first_name, last_name=last_name)
+                    #Login after registrer
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
+
+
 
     else:
         return render(request, 'accounts/register.html')
