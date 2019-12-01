1e33cd2efd1ccd0ef0a4e590a1f78f1386547672
dm1tro69 <dimolg22@gmail.com>
2019-12-01 22:55:47 +0200

    finish

--- btre/settings.py
+++ btre/settings.py
@@ -153,3 +153,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dm1tr0pyth@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
