61913c44cff6372c59beba0819d16163f7a42902
dm1tro69 <dimolg22@gmail.com>
2019-12-02 20:40:59 +0200

    disable email

--- contacts/views.py
+++ contacts/views.py
@@ -31,13 +31,13 @@ def contact(request):
 
         #Send email
 
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing + '.Sign into the admin panel for more info',
-            'dimolg22@gmail.com',
-            [realtor_email, 'techguyinfo@gmail.com'],
-            fail_silently=False
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing + '.Sign into the admin panel for more info',
+        #     'dimolg22@gmail.com',
+        #     [realtor_email, 'techguyinfo@gmail.com'],
+        #     fail_silently=False
+        # )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
